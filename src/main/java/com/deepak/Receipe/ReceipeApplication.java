22d14035c72b32d6d5add09c82eb582ed1073954
Deepak Sriram <dsriram@griddynamics.com>
2024-12-13 16:48:52 +0530

    Initialized Receipe Project

---
+++ src/main/java/com/deepak/Receipe/ReceipeApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.Receipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepak/Receipe/ReceipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.Receipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
